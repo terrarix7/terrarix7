28caf891d7defeb0a6c81366b7866fe69a2df8fc
terrarix7 <terrarix7@gmail.com>
2025-07-08 16:11:59 +0530

    remove unwanted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Source_Code_Pro, Space_Mono } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 import Script from "next/script";
 
-// Source Code Pro
 import "./globals.css";
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const spaceMono = Space_Mono({
   variable: "--font-space-mono",
